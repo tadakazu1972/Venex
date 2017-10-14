6df9170e31764749b57d000201099d7930277bec
中道忠和 <tadakazu@chuudou-no-mac-mini.local>
2017-10-14 12:18:47 +0900

    change dialog height

--- Venex/DialogSeria1.swift
+++ Venex/DialogSeria1.swift
@@ -45,7 +45,7 @@ class DialogSeria1: NSObject, UITableViewDelegate, UITableViewDataSource {
         //初期設定
         //Win1
         win1.backgroundColor = UIColor.white
-        win1.frame = CGRect(x: 80,y: 200,width: parent.view.frame.width-40,height: parent.view.frame.height-400)
+        win1.frame = CGRect(x: 80,y: 200,width: parent.view.frame.width-40,height: 200)
         win1.layer.position = CGPoint(x: parent.view.frame.width/2, y: parent.view.frame.height/2)
         win1.alpha = 1.0
         win1.layer.cornerRadius = 10
